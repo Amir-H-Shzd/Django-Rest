d2bc5b3bd9b665c80d51b38b6358b113a66562e1
Amir-H-Shzd <amirshir737@gmail.com>
2022-07-23 01:23:10 +0430

    started Djangorest
    using DjangoRest quick start guide

---
+++ mysite/serializers.py
@@ -0,0 +1,10 @@
+import django
+
+
+from django.contrib.auth.models import User,Group
+from rest_framework import serializers
+
+class UserSerializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
